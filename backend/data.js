e8619af532ef0bd851709db79d32f01e101a2589
Simon Pettersson <simon.pettersson@protonmail.com>
2019-11-18 12:29:00 +0100

    created app

---
+++ backend/data.js
@@ -0,0 +1,15 @@
+// backend/data.js
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// this will be our database's data structure
+const DataSchema = new Schema(
+	{
+		id: Number,
+		message: String
+	},
+	{ timestamps: true }
+);
+	
+// export the new Schema so we can modify it using Node.js
+module.exports = mongoose.model("Data", DataSchema);
